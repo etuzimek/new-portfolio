93f6f9443f1968391157200cdbbc5e6cae7e4173
qazwaz <edwardtuzimek@gmail.com>
2023-04-17 09:16:26 +0200

    Projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
 import './projects.css'
+import IMG1 from '../../assets/me-about.jpg'
+import IMG2 from '../../assets/oldme.png'
+import IMG3 from '../../assets/oldme.png'
+import IMG4 from '../../assets/oldme.png'
+import IMG5 from '../../assets/oldme.png'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Sea Apartment',
+        github: 'https://github.com/qazwaz/apartament3',
+        demo: 'https://silver-fox-e6696b.netlify.app/'
+    },
+    {
+        id: 2,
+        image: IMG1,
+        title: 'Old Portfolio',
+        github: 'https://github.com/qazwaz/apartament3',
+        demo: 'https://silver-fox-e6696b.netlify.app/'
+    },
+    {
+        id: 3,
+        image: IMG1,
+        title: 'Start Helping',
+        github: 'https://github.com/qazwaz/apartament3',
+        demo: 'https://silver-fox-e6696b.netlify.app/'
+    },
+    {
+        id: 4,
+        image: IMG1,
+        title: 'Best Shop',
+        github: 'https://github.com/qazwaz/apartament3',
+        demo: 'https://silver-fox-e6696b.netlify.app/'
+    },
+    {
+        id: 5,
+        image: IMG1,
+        title: 'Car Sharing',
+        github: 'https://github.com/qazwaz/apartament3',
+        demo: 'https://silver-fox-e6696b.netlify.app/'
+    }
+]
 
 const Projects = () => {
     return (
-        <section id="projects">Projects</section>
+        <section id="projects">
+            <h5>My Recent Work</h5>
+            <h2>Projects</h2>
+
+            <div className="container portfolio__container">
+                {
+                    data.map(({ id, image, title, github, demo }) => {
+                        return (
+                            <article key={id} className='portfolio__item'>
+                                <div className="portfolio__itme-image">
+                                    <img src={image} alt={title} />
+                                </div>
+                                <h3>{title}</h3>
+                                <div className="portfolio__item-cta">
+                                    <a href={github} className='btn' target='_blank'>Github</a>
+                                    <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
