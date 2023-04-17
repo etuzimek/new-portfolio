544be81d2932e1b4ab9d27b7d53d07769d25aad4
qazwaz <edwardtuzimek@gmail.com>
2023-04-17 11:36:07 +0200

    Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#" className='footer__logo'>ETUZ</a>
+            <ul className='permalinks'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://facebook.com"><FaFacebookF /></a>
+                <a href="https://twitter.com"><IoLogoTwitter /></a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; ETUZ all rights reserved</small>
+            </div>
+        </footer>
     )
 }
 
